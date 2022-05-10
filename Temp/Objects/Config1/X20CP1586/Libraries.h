7399c4869f2860a7430e657bf47bd9bf02557670
Brian Ng <choonkiat8@gmail.com>
2022-05-10 16:07:26 +0800

    no message

--- Temp/Objects/Config1/X20CP1586/Libraries.h
+++ Temp/Objects/Config1/X20CP1586/Libraries.h
@@ -1,8 +1,8 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
 
-#ifndef _LIBRARIES_20220510154142_
-#define _LIBRARIES_20220510154142_
+#ifndef _LIBRARIES_20220510160635_
+#define _LIBRARIES_20220510160635_
 
 __asm__(".section \".plc\"");
 
@@ -479,5 +479,5 @@ __asm__(".ascii \"iecfile \\\"Logical/Libraries/AsBrStr/AsBrStr.var\\\" scope \\
 __asm__(".previous");
 
 
-#endif /* _LIBRARIES_20220510154142_ */
+#endif /* _LIBRARIES_20220510160635_ */
 
