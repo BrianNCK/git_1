52b6526d8a5f20d7463efcde582a89e0fe51f555
lihui <LIHUI01.20@ichat.sp.edu.sg>
2022-05-18 15:07:18 +0800

    ignoreee

--- Temp/Objects/Config1/X20CP1586/Libraries.h
+++ Temp/Objects/Config1/X20CP1586/Libraries.h
@@ -1,8 +1,8 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
 
-#ifndef _LIBRARIES_20220517092740_
-#define _LIBRARIES_20220517092740_
+#ifndef _LIBRARIES_20220518150427_
+#define _LIBRARIES_20220518150427_
 
 __asm__(".section \".plc\"");
 
@@ -479,5 +479,5 @@ __asm__(".ascii \"iecfile \\\"Logical/Libraries/AsBrStr/AsBrStr.var\\\" scope \\
 __asm__(".previous");
 
 
-#endif /* _LIBRARIES_20220517092740_ */
+#endif /* _LIBRARIES_20220518150427_ */
 
